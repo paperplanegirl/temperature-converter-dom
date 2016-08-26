16d69e3b571333147249d03dfdf982929d557b87
Yvonne Chia <yvonnechia@Yvonnes-MacBook.local>
2016-08-26 17:01:45 +0800

    fixed javascript indentation

--- js/script.js
+++ js/script.js
@@ -3,66 +3,62 @@ console.log('Hello, front end');
 document.addEventListener("DOMContentLoaded", function(event) {
   console.log("DOM fully loaded and parsed");
 
+  var tempType;
+  document.getElementById("submit").addEventListener('click', function() {
+    if (tempType === "c") {
+      toCelsius()
+    }
+    else if (tempType === "f") {
+      toFahrenheit()
+    }
+  })
 
-var tempType;
+  console.log("it's working")
 
-document.getElementById("submit").addEventListener('click', function() {
-  if (tempType === "c") {
-    toCelsius()
+  function toFahrenheit() {
+    var temp = document.getElementById("input").value
+    var fahResults = (temp * (9/5)) + 32
+    document.getElementById("result").textContent = fahResults
   }
-  else if (tempType === "f") {
-    toFahrenheit()
+
+  function toCelsius() {
+    var temp = document.getElementById("input").value
+    var celResults = (temp - 32) * 5/9
+    document.getElementById("result").textContent = celResults
   }
-})
 
-console.log("it's working")
+  document.getElementsByClassName("tempSelect")[0].addEventListener('click', function() {
+    clearResults();
+    tempType = "f"
+    console.log(tempType);
+  })
 
-function toFahrenheit() {
-  var temp = document.getElementById("input").value
-  var fahResults = (temp * (9/5)) + 32
-  document.getElementById("result").textContent = fahResults
-}
+  document.getElementsByClassName("tempSelect")[1].addEventListener('click', function() {
+    clearResults();
+    tempType = "c"
+    console.log(tempType);
+  })
 
-function toCelsius() {
-  var temp = document.getElementById("input").value
-  var celResults = (temp - 32) * 5/9
-  document.getElementById("result").textContent = celResults
-}
-
-document.getElementsByClassName("tempSelect")[0].addEventListener('click', function() {
-  clearResults();
-  tempType = "f"
-  console.log(tempType);
-})
-
-document.getElementsByClassName("tempSelect")[1].addEventListener('click', function() {
-  clearResults();
-  tempType = "c"
-  console.log(tempType);
-})
-
-document.getElementById("submit").addEventListener('click', function() {
-  if (tempType = "c")
-    // if degree C selected then this:
-    document.getElementById("input").innerHTML = toFahrenheit();
-  else if (tempType = "f")
-    //else if degree F selected then this:
-    document.getElementById("input").innerHTML = toCelsius();
-  else
-    console.log("Wrong Option");
-});
+  document.getElementById("submit").addEventListener('click', function() {
+    if (tempType = "c")
+      // if degree C selected then this:
+      document.getElementById("input").innerHTML = toFahrenheit();
+    else if (tempType = "f")
+      //else if degree F selected then this:
+      document.getElementById("input").innerHTML = toCelsius();
+    else
+      console.log("Wrong Option");
+  });
 
 
+  console.log("pls work")
 
-console.log("pls work")
+  // creating function for clearing page
+  document.getElementById("clear").addEventListener('click',clearResults)
 
-
-// creating function for clearing page
-document.getElementById("clear").addEventListener('click',clearResults)
-
-function clearResults() {
-  document.getElementById("result").textContent='';
-  document.getElementById("input").value='';
-}
+  function clearResults() {
+    document.getElementById("result").textContent='';
+    document.getElementById("input").value='';
+  }
 
 });
